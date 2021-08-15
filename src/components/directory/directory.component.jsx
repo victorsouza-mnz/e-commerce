5a073f28e74a5f266e5557cb45f54847e7a1c451
victorsouza-mnz <victorsouzamnz@gmail.com>
2021-08-15 18:08:09 -0300

    styling homepage

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -30,7 +30,7 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: 'shop/women'
                 },
                 {
                   title: 'mens',
@@ -48,8 +48,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                  this.state.sections.map(section => (
-                    <MenuItem title = {section.title} />
+                  this.state.sections.map(({title, imageUrl, id, size}) => (
+                    <MenuItem key={id} imageUrl={imageUrl} title={title} size={size} />
                 ))
                 }
             </div>
