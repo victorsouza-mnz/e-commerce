4eb0ca8f71e552dccfb1d44c785b5f9cd56db01d
victorsouza-mnz <victorsouzamnz@gmail.com>
2021-08-16 01:54:58 -0300

    navegate withRouter in menu items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -48,8 +48,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                  this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} imageUrl={imageUrl} title={title} size={size} />
+                  this.state.sections.map(({id, ...otherProps}) => (
+                    <MenuItem key={id} {...otherProps} />
                 ))
                 }
             </div>
