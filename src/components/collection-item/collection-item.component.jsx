c9308f753077c3f5502b1661bca4e627dcef5db1
victorsouza-mnz <victorsouzamnz@gmail.com>
2021-08-25 16:31:43 -0300

    update cart data with new data and upgrade cart utils to group items in cart array

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import './collection-item.styles.scss'
+import CustomButton from '../custom-button/custom-button.component'
+import { connect } from 'react-redux'
+import { addItem } from '../../redux/cart/cart.actions' 
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+    const {name, price, imageUrl} = item
+    return(
     <div className="collection-item">
         <div
             className="image"
@@ -17,9 +22,14 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
                 {price}
             </span>
         </div>
+        <CustomButton inverted onClick={ () => addItem(item) } >Add to cart</CustomButton>
 
     </div>
-)
+)}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
 
 
-export default CollectionItem
+export default connect(null, mapDispatchToProps)(CollectionItem)
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,3 +1,4 @@
 export const CartActionTypes = {
-    TOGGLE_CART_ACTIVE : 'TOGGLE_CART_ACTIVE'
+    TOGGLE_CART_ACTIVE : 'TOGGLE_CART_ACTIVE',
+    ADD_ITEM: 'ADD_ITEM'
 }
